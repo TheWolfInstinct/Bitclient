859c7fbdd5ba86fae8ebb03028736f50bf5bfb1a
Fixer <fixer3838@live.fr>
2019-12-04 03:53:32 +0100

    Changed the way the connection buffer is built

--- src/tracker.rs
+++ src/tracker.rs
@@ -6,7 +6,6 @@ use std::net::UdpSocket;
 extern crate rand;
 use rand::Rng;
 
-use bytebuffer::ByteBuffer;
 
 pub fn make_url(torrent_announce: String) -> String {
     let url = Url::parse(&torrent_announce[..]).expect("Couldn't parse url");
@@ -14,33 +13,29 @@ pub fn make_url(torrent_announce: String) -> String {
     socket_url
 }
 
-pub fn make_connect_request() -> String {
+pub fn make_connect_request() -> Vec<u8> {
     let mut rng = rand::thread_rng();
-    //let mut buf: Vec<String> = vec![String::new(); 4];
-    //buf[0] = String::from("000417");
-    //buf[1] = String::from("27101980");
-    //buf[2] = String::from("00000000");
-    //buf[3] = rng.gen::<u32>().to_string();
+    let mut buf: Vec<u8> = vec![0x00, 0x00, 0x04, 0x17, 0x27, 0x10, 0x19, 0x80, 0x00, 0x00, 0x00, 0x00, rng.gen::<u8>(), rng.gen::<u8>(), rng.gen::<u8>(), rng.gen::<u8>()];
     //let connect_message: String = buf.into_iter().collect();
-    let mut buffer = ByteBuffer::new();
-    buffer.write_bytes(&vec![0x
-
-    connect_message
+    //let mut buffer = ByteBuffer::new();
+    //buffer.write_bytes(&vec![0x000041727101980, 0x00000000]);
+    //buffer.write_u32(rng.gen::<u32>());
+    buf
 }
 
 
-pub fn udp_send(socket: UdpSocket, message: String, torrent_announce: String) {
+pub fn udp_send(socket: UdpSocket, buf: Vec<u8>, torrent_announce: String) {
     let socket_url = make_url(torrent_announce);
     println!("socket_url: {:?}", socket_url);
     let mut addrs_iter = socket_url.to_socket_addrs().expect("Error transforming the address into a SocketAddr");
     let next_ip = addrs_iter.next().expect("couldn't get next ip in the ip iterator");
     println!("next_ip: {:?}", next_ip);
     socket.connect(next_ip).expect("Couldn't connect to the ip address with the socket"); 
-    println!("{:?}", message);
-    let buf = message.as_bytes();
     println!("{:?}", buf);
+    let message = &buf[..];
+    println!("{:?}", message);
 
-    match socket.send(buf) {
+    match socket.send(message) {
         Ok(number_of_bytes) => {
             println!("Number of bytes sent: {:?}", number_of_bytes);
             let mut buf = [0; 10];
