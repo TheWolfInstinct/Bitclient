6ead9b4b1175a20113432f5fe4c6260c1838a02d
Fixer <fixer3838@live.fr>
2020-01-22 01:18:18 +0100

    Added parser for connection response

--- src/tracker.rs
+++ src/tracker.rs
@@ -15,12 +15,19 @@ pub fn make_url(torrent_announce: String) -> String {
 
 pub fn make_connect_request() -> Vec<u8> {
     let mut rng = rand::thread_rng();
-    let mut buf: Vec<u8> = vec![0x00, 0x00, 0x04, 0x17, 0x27, 0x10, 0x19, 0x80, 0x00, 0x00, 0x00, 0x00, rng.gen::<u8>(), rng.gen::<u8>(), rng.gen::<u8>(), rng.gen::<u8>()];
+    let buf: Vec<u8> = vec![0x00, 0x00, 0x04, 0x17, 0x27, 0x10, 0x19, 0x80, 0x00, 0x00, 0x00, 0x00, rng.gen::<u8>(), rng.gen::<u8>(), rng.gen::<u8>(), rng.gen::<u8>()];
     //let connect_message: String = buf.into_iter().collect();
     //let mut buffer = ByteBuffer::new();
     //buffer.write_bytes(&vec![0x000041727101980, 0x00000000]);
     //buffer.write_u32(rng.gen::<u32>());
     buf
+}
+
+pub fn parse_connect_response(buf: [u8; 16]) {
+    let action = &buf[0..4];
+    let transaction_id = &buf[4..8];
+    let connection_id = &buf[8..16];
+    println!("action: {:?}, transaction_id: {:?}, connection_id: {:?}", action, transaction_id, connection_id);
 }
 
 
@@ -38,9 +45,10 @@ pub fn udp_send(socket: UdpSocket, buf: Vec<u8>, torrent_announce: String) {
     match socket.send(message) {
         Ok(number_of_bytes) => {
             println!("Number of bytes sent: {:?}", number_of_bytes);
-            let mut buf = [0; 10];
+            let mut buf = [0; 16];
             let (amt, src) = socket.recv_from(&mut buf).expect("Couldn't receive data from the target");
-            println!("Amount: {:?}, source: {:?}", amt, src);
+            parse_connect_response(buf);
+            println!("Amount: {:?}, source: {:?}, buffer: {:?}", amt, src, buf);
         }
         Err(err) => println!("Error: {:?}", err), 
     }
